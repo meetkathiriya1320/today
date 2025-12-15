e0637777926dfb2412ea9485125fa919e93bf592
meetkathiriya1320 <meet.kathiriya1320@gmail.com>
2025-12-15 11:42:16 +0530

    changes in index

--- src/config/config.js
+++ src/config/config.js
@@ -1,19 +1,10 @@
-import dotenv from 'dotenv';
-dotenv.config();
+import { Sequelize } from 'sequelize';
 
-const isProduction = process.env.NODE_MODE === 'production';
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  dialect: process.env.DB_DIALECT,
+  logging: console.log,
+});
 
-export default {
-    username: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_NAME,
-    host: isProduction
-        ? process.env.DB_HOST              // internal Railway host
-        : 'containers-us-west-123.railway.app', // local/public host
-    port: Number(process.env.DB_PORT) || 5432,
-    dialect: process.env.DB_DIALECT || 'postgres',
-    logging: false,
-    dialectOptions: {
-        connectTimeout: 30000, // optional: wait 30s for DB
-    }
-};
+export default sequelize;

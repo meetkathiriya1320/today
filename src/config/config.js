8b33eee577047c8eb8052a6b7152cf76204d56ba
meetkathiriya1320 <meet.kathiriya1320@gmail.com>
2025-12-15 11:43:36 +0530

    changes in index

--- src/config/config.js
+++ src/config/config.js
@@ -1,10 +1,14 @@
-import { Sequelize } from 'sequelize';
+import dotenv from 'dotenv';
+dotenv.config();
 
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  dialect: process.env.DB_DIALECT,
-  logging: console.log,
-});
-
-export default sequelize;
+export default {
+    development: {
+        username: process.env.DB_USER,
+        password: process.env.DB_PASS,
+        database: process.env.DB_NAME,
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT,
+        dialect: process.env.DB_DIALECT,
+        logging: false
+    }
+};
